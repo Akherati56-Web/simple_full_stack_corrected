82971f2744721387e9661d6440f330ccbd32f38a
akherati56 <akherati5660@gmail.com>
2023-11-24 21:38:36 +0330

    config laravel websocket

--- backend/BookStore/routes/api.php
+++ backend/BookStore/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
