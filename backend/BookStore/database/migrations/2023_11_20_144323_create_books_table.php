e3a9a72d2bee34a9d29f7b0a64219f8e38e0a37d
akherati56 <akherati5660@gmail.com>
2023-11-25 17:06:20 +0330

    update

--- backend/BookStore/database/migrations/2023_11_20_144323_create_books_table.php
+++ backend/BookStore/database/migrations/2023_11_20_144323_create_books_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();;
             $table->string('name')->nullable();
-            $table->string('title')->nullable();;
+            $table->string('title')->nullable();
             $table->string('description')->nullable();;
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
