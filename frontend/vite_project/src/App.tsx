3a5dacdcc1781810f0edff6b89b30e372b74806e
akherati56 <akherati5660@gmail.com>
2023-11-30 10:53:55 +0330

    config all backend to run in docker on server using seperate nginx, now i need to config frontend and use reverse proxy as well

--- frontend/vite_project/src/App.tsx
+++ frontend/vite_project/src/App.tsx
@@ -1,11 +1,8 @@
- 
-import About from './Components/views/Home/About';
 import Content from './Components/views/Home/Content';
 import Home from './Components/views/Home/Home' 
 import Layout from './Components/views/Home/Layout';
 import './style.scss'; 
  
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 
 export default function App() {
@@ -21,5 +18,7 @@ export default function App() {
   );
 }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
+/* 
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
+ */
