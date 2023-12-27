736af2ec7111b7ebbad5c4d0dc63b5b2b104f802
akherati56 <akherati5660@gmail.com>
2023-12-28 02:21:16 +0330

    update

--- backend/BookStore/routes/web.php
+++ backend/BookStore/routes/web.php
@@ -21,6 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/temp', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
