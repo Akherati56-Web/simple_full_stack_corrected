41399f4d14f5526a92c5eb3efe1840d2d1437bf2
akherati56 <akherati5660@gmail.com>
2024-01-14 14:57:07 +0330

    update

--- ai/tutorial/settings.py
+++ ai/tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-opjajy)(5dan^tkn$hgf!^f$irh)i7u!-9e9bf)i_=7rjnc4k0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['phpmyadmin.akherati5660.com']
 
 
 # Application definition
