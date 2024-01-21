dcd31bc0d6d3227c8db0b7249749c0c9f5d77e61
akherati56 <akherati5660@gmail.com>
2024-01-21 11:31:29 +0330

    update

--- frontend/vite_project/vite.config.ts
+++ frontend/vite_project/vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true
+        /* other options */
+      },
+      manifest: {
+        icons: [
+          {
+            src: "/icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: true,
     port: 3000, // This is the port which we will use in docker
