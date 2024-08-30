4ac0c88d54802bae614ec9d4f7d441d3adba085a
akherati56 <akherati5660@gmail.com>
2024-08-30 18:50:35 +0330

    feat(AI): add digit detector model, and add django api to use it

--- ai/AI/digit_recognition/views.py
+++ ai/AI/digit_recognition/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-import numpy as np
-import tensorflow as tf
-from tflite_runtime.interpreter import Interpreter
 from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-import json
+import numpy as np
+import io
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
 
 @method_decorator(csrf_exempt, name='dispatch')
 class DigitRecognitionView(View):
@@ -21,15 +21,17 @@ class DigitRecognitionView(View):
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-    def get(self, request, *args, **kwargs):
-        return 'test'
-
     def post(self, request, *args, **kwargs):
         try:
-            # Parse input data
-            data = json.loads(request.body)
-            image = np.array(data['image'], dtype=np.float32)
-            image = image.reshape((1, 28, 28, 1))  # Reshape for the model input
+            # Handle file upload
+            uploaded_file = request.FILES['image']
+            image = Image.open(uploaded_file)
+            image = image.convert('L')  # Ensure image is in grayscale
+            image = image.resize((28, 28))  # Resize to match model input
+            image = np.array(image).astype(np.float32) / 255.0  # Normalize
+
+            # Reshape to match the expected input shape: [28, 28, 1]
+            image = image.reshape((28, 28, 1))
 
             # Set the tensor to point to the input data to be inferred
             self.interpreter.set_tensor(self.input_details[0]['index'], image)
