dec1d0fd6a6cf1c2d1193c4aed0ddcee781a700c
akherati56 <akherati5660@gmail.com>
2024-08-30 17:14:24 +0330

    update

--- ai/ai/tutorial/settings.py
+++ ai/ai/tutorial/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myproject',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'postgres-database-service',
-        # 'HOST': 'localhost',
-        'PORT': '5432',
-    },
-    'djongo': {
-        'ENGINE': 'djongo',
-        'NAME': 'bezkoder_db',
-        # 'HOST': 'mongo-database-service', # k8s database name
-        'HOST': 'localhost',
-        'PORT': 27017,
-    },
-    'sqllite': {
+    # 'default':{
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'myproject',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'postgres-database-service',
+    #     # 'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
+    # 'djongo': {
+    #     'ENGINE': 'djongo',
+    #     'NAME': 'bezkoder_db',
+    #     # 'HOST': 'mongo-database-service', # k8s database name
+    #     'HOST': 'localhost',
+    #     'PORT': 27017,
+    # },
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
